c5b9af0a925d519f2fe5c69f1b6e43e2db8993b3
Dennis M Webb <denniswebb@yahoo.com>
2022-11-17 21:37:18 +0000

    Adding Character Classes. This will break the  build

---
+++ LevelUpGame/levelup/Character/TestCharacter.cs
@@ -0,0 +1,24 @@
+namespace levelup
+{
+    public class Character {
+    string DEFAULT_CHARACTER_NAME = "Zelda";
+    public string Name {get; set;}
+
+    public TestCharacter()
+    {
+        Name = DEFAULT_CHARACTER_NAME;
+    }
+    public Character(string name)
+    {
+        //Name = name;
+    }
+    public void enterMap(GameMap map) 
+    {
+        //TODO
+    }
+
+    public Position getPosition() 
+    {
+        //TODO
+    }
+}
