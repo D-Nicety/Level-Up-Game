4b8f5cc9c1a8788a7b5ef8e80be329c3efae6f0a
Dennis M Webb <denniswebb@yahoo.com>
2022-11-17 23:49:22 +0000

    Working on unit tests

---
+++ LevelUpGame.Tests/levelup/CharacterTest/CharacterTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using levelup;
+
+namespace levelup
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        private Character? testObj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new Character();
+        }
+
+        [Test]
+        public void IsDefaultCharacterInitialized()
+        {
+#pragma warning disable CS8602 // Rethrow to preserve stack details
+            Assert.IsNotNull(testObj.Name);
+        }
+    }
+}
